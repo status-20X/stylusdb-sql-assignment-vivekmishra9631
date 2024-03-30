54d84c286e61ecc8f1f2631757c35511c8fb4326
vivekmishra9631 <vivekmishra.211ch059@gmail.com>
2024-03-30 12:26:35 +0530

    steps 1, 2 and 3

--- tests/step-02/index.test.js
+++ tests/step-02/index.test.js
@@ -2,6 +2,7 @@ const readCSV = require('../../src/csvReader');
 
 test('Read CSV File', async () => {
     const data = await readCSV('./sample.csv');
+    console.log(data);
     expect(data.length).toBeGreaterThan(0);
     expect(data.length).toBe(3);
     expect(data[0].name).toBe('John');
