6f3c390dbbbc978273802acf63decfd8e56aadc0
vivekmishra9631 <vivekmishra.211ch059@gmail.com>
2024-05-06 15:45:51 +0530

    step08 verification

--- tests/step-02/index.test.js
+++ tests/step-02/index.test.js
@@ -1,7 +1,7 @@
 const readCSV = require('../../src/csvReader');
 
 test('Read CSV File', async () => {
-    const data = await readCSV('./sample.csv');
+    const data = await readCSV('./student.csv');
     console.log(data);
     expect(data.length).toBeGreaterThan(0);
     expect(data.length).toBe(3);
